d10b2358a88df9f3249ec3d49ca018246b9948e0
y1feng200156 <charles.tanchao@gmail.com>
2013-01-28 08:24:28 +0800

    修改应用权限表

--- DBFile/aut.sql
+++ DBFile/aut.sql
@@ -7,6 +7,7 @@ MySQL - 5.1.28-rc-community
 
 create table `app_authority` (
 	`id` double ,
-	`zname` varchar (600),
-	`ename` varchar (600)
+	`zname` varchar (1800),
+	`ename` varchar (1800)
 ); 
+insert into `app_authority` (`id`, `zname`, `ename`) values('1','应用列表','appInfoAction-appList');
